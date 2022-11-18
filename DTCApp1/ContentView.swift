6b1d9c17e75c3d89af5dbb6f1c2652971ac44cab
Matthew Brown <52255000+mbrown98@users.noreply.github.com>
2022-11-18 17:37:28 -0500

    Initial Commit

---
+++ DTCApp1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DTCApp1
+//
+//  Created by Matthew Brown on 11/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DTCApp1/DTCApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  DTCApp1App.swift
+//  DTCApp1
+//
+//  Created by Matthew Brown on 11/18/22.
+//
+
+import SwiftUI
+
+@main
+struct DTCApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
